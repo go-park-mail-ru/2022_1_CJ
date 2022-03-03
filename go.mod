3e67cec1d4db7faaa6d93bfa412ae25e4fcf9a6d
papazloynt <cool.chastikov53@gmail.com>
2022-03-03 21:19:25 +0300

    add models

---
+++ go.mod
@@ -0,0 +1,11 @@
+module 2022_1_CJ
+
+go 1.17
+
+require (
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	go.mongodb.org/mongo-driver v1.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
---
+++ internal/app/model/user.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User ...
+type User struct {
+	ID   int    `json:"_id"`
+	Name string `json:"name"`
+
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+
+	Password          string `json:"password"`
+	EncryptedPassword string `json:"-"`
+}
+
+// Validate ...
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required),
+	)
+}
+
+// BeforeCreate ...
+func (u *User) BeforeCreate() error {
+	if len(u.Password) > 0 {
+		enc, err := encryptString(u.Password)
+		if err != nil {
+			return err
+		}
+		u.EncryptedPassword = enc
+	}
+
+	return nil
+}
+
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
