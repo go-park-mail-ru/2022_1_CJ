ce806847a46a6d061fa5f9b085a6c6eee588456c
rinatkh <khairullinrinat.99@gmail.com>
2022-03-08 21:03:11 +0300

    SJ-9 fix 0.0.2

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -1,11 +1,15 @@
 package apiserver
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
+	"github.com/sirupsen/logrus"
 	"net/http"
+	"time"
 
 	"github.com/go-park-mail-ru/2022_1_CJ/internal/app/model"
+	"github.com/gorilla/handlers"
 
 	"github.com/go-park-mail-ru/2022_1_CJ/internal/app/store"
 	"github.com/gorilla/mux"
@@ -13,15 +17,21 @@ import (
 )
 
 const (
-	sessionName = "cj_team"
+	sessionName        = "cj_team"
+	ctxKeyUser  ctxKey = iota
+	ctxKeyRequestID
 )
 
 var (
 	errIncorrectEmailOrPassword = errors.New("incorrect email or password")
+	errNotAuthenticated         = errors.New("not authenticated")
 )
+
+type ctxKey int8
 
 type server struct {
 	router       *mux.Router
+	logger       *logrus.Logger
 	store        store.Store
 	sessionStore sessions.Store
 }
@@ -29,6 +39,7 @@ type server struct {
 func newServer(store store.Store, sessionStore sessions.Store) *server {
 	s := &server{
 		router:       mux.NewRouter(),
+		logger:       logrus.New(),
 		store:        store,
 		sessionStore: sessionStore,
 	}
@@ -43,8 +54,69 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) configureRouter() {
-	s.router.HandleFunc("/register", s.handleUsersCreate())
-	s.router.HandleFunc("/auth", s.handleSessionsCreate())
+	s.router.Use(s.logRequest)
+	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
+	s.router.HandleFunc("/api/register", s.handleUsersCreate()).Methods("POST")
+	s.router.HandleFunc("/api/auth", s.handleSessionsCreate()).Methods("POST")
+
+	private := s.router.PathPrefix("/api/private").Subrouter()
+	private.Use(s.authenticateUser)
+	private.HandleFunc("/api/feed", s.handleFeed()).Methods("GET")
+}
+
+func (s *server) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := s.logger.WithFields(logrus.Fields{
+			"remote_addr": r.RemoteAddr,
+			"request_id":  r.Context().Value(ctxKeyRequestID),
+		})
+		logger.Infof("started %s %s", r.Method, r.RequestURI)
+
+		start := time.Now()
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
+
+		var level logrus.Level
+		switch {
+		case rw.code >= 500:
+			level = logrus.ErrorLevel
+		case rw.code >= 400:
+			level = logrus.WarnLevel
+		default:
+			level = logrus.InfoLevel
+		}
+		logger.Logf(
+			level,
+			"completed with %d %s in %v",
+			rw.code,
+			http.StatusText(rw.code),
+			time.Now().Sub(start),
+		)
+	})
+}
+
+func (s *server) authenticateUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, err := s.sessionStore.Get(r, sessionName)
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		id, ok := session.Values["user_id"]
+		if !ok {
+			s.error(w, r, http.StatusUnauthorized, errNotAuthenticated)
+			return
+		}
+
+		u, err := s.store.User().Find(id.(int))
+		if err != nil {
+			s.error(w, r, http.StatusUnauthorized, errNotAuthenticated)
+			return
+		}
+
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyUser, u)))
+	})
 }
 
 func (s *server) handleUsersCreate() http.HandlerFunc {
@@ -110,6 +182,12 @@ func (s *server) handleSessionsCreate() http.HandlerFunc {
 		}
 
 		s.respond(w, r, http.StatusOK, nil)
+	}
+}
+
+func (s *server) handleFeed() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		s.respond(w, r, http.StatusOK, r.Context().Value(ctxKeyUser).(*model.User))
 	}
 }
 
--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -5,5 +5,6 @@ import "github.com/go-park-mail-ru/2022_1_CJ/internal/app/model"
 // UserRepository ...
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
