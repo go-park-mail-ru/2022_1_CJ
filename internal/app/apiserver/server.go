568145ee252ffdbcf7a14d9a278b995159669442
rinatkh <khairullinrinat.99@gmail.com>
2022-03-08 18:48:17 +0300

    add swagger

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -43,8 +43,8 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) configureRouter() {
-	s.router.HandleFunc("/users", s.handleUsersCreate())
-	s.router.HandleFunc("/sessions", s.handleSessionsCreate())
+	s.router.HandleFunc("/register", s.handleUsersCreate())
+	s.router.HandleFunc("/auth", s.handleSessionsCreate())
 }
 
 func (s *server) handleUsersCreate() http.HandlerFunc {
